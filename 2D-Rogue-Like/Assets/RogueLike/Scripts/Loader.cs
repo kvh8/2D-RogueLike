640bea76763fd9bed84b8fc0593a2525bdc16465
kvh8 <54556865+kvh8@users.noreply.github.com>
2020-02-07 10:07:13 -0800

    fixing errors in scripts adding sound and touch capability

--- 2D-Rogue-Like/Assets/RogueLike/Scripts/Loader.cs
+++ 2D-Rogue-Like/Assets/RogueLike/Scripts/Loader.cs
@@ -1,22 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
-
-public class Loader : MonoBehaviour
+namespace Completed
 {
-    public GameObject gameManager;            //GameManager prefab to instantiate.
-   /* public GameObject soundManager; */           //SoundManager prefab to instantiate.
+	public class Loader : MonoBehaviour
+	{
+		public GameObject gameManager;          //GameManager prefab to instantiate.
+		public GameObject soundManager;         //SoundManager prefab to instantiate.
 
 
-    void Awake()
-    {
-        //Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
-        if (GameManager.instance == null)
+		void Awake()
+		{
+			//Check if a GameManager has already been assigned to static variable GameManager.instance or if it's still null
+			if (GameManager.instance == null)
 
-            //Instantiate gameManager prefab
-            Instantiate(gameManager);
+				//Instantiate gameManager prefab
+				Instantiate(gameManager);
 
+			//Check if a SoundManager has already been assigned to static variable GameManager.instance or if it's still null
+			if (SoundManager.instance == null)
 
-    }
+				//Instantiate SoundManager prefab
+				Instantiate(soundManager);
+		}
+	}
 }
